8120a037139490df25c9381ce736747433ec651e
Valdouz <55506883+Valdouz@users.noreply.github.com>
2021-01-16 17:01:53 +0100

    Embed 2 by Haleloch

--- Commandes/avatar.js
+++ Commandes/avatar.js
@@ -9,23 +9,27 @@ module.exports.run = (client, message, args) => {
     .setTimestamp()
     .setColor('#c215e9')
     .setTitle('Voici votre avatar :')
-    .setThumbnail(`${message.author.displayAvatarURL()}`)
+    .setImage(`${message.author.displayAvatarURL()}`)
     .setFooter("De AnotherBot pour " + message.author.username, message.author.displayAvatarURL());
     return message.channel.send(embed)
     
     }
     
     
-    const avatarList = message.mentions.users.map(user => {
+ const avatarList = message.mentions.users.map(user => {
     
-    return `${user.username}\'s avatar: ${user.displayAvatarURL()}`;
-    
+    const embed = new Discord.MessageEmbed()
+    .setTimestamp()
+    .setColor('#c215e9')
+    .setTitle(`Voici l\'avatar de : ${user.username}`)
+    .setImage(`${user.displayAvatarURL()}`)
+    .setFooter("De AnotherBot pour " + message.author.username, message.author.displayAvatarURL());
+
+     message.channel.send(embed)
     });
-    
-    message.channel.send(avatarList);
     }
 
 
 module.exports.help = {
     name: 'avatar',
-  };  
+  };
