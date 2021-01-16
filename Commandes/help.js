12955faea6bc38b0a044998935393f9d5b24bdd8
Voxfy <55506883+Valdouz@users.noreply.github.com>
2021-01-16 22:00:27 +0100

    add emoji on .help command

--- Commandes/help.js
+++ Commandes/help.js
@@ -7,7 +7,7 @@ const embed = new Discord.MessageEmbed()
 .setTimestamp() 
 .setColor('#FFC0CB') 
 .setTitle('Liste des commandes ') 
-.setDescription("**Fun:**\n`avatar` : permet d'afficher la photo de profil de quelqu'un.\n`ping` : affiche le ping.\n`test` : commande de test.\n\n**Moderation**\n`kick` : permet de kick un utilisateur.\n`ban` : permet de ban un utilisateur.\n`clear` : Permet de clear un nombre de messages entre 1 et 100\n\n**Le prefix est : `.`**")
+.setDescription(":fire: **Fun:**\n`avatar` : permet d'afficher la photo de profil de quelqu'un.\n`ping` : affiche le ping.\n`test` : commande de test.\n\n:tools: **Moderation**\n`kick` : permet de kick un utilisateur.\n`ban` : permet de ban un utilisateur.\n`clear` : Permet de clear un nombre de messages entre 1 et 100\n\n:round_pushpin: **Le prefix est : `.`**")
 .setFooter("De AnotherBot pour " + message.author.username,
 message.author.displayAvatarURL()); 
 
