fbd632cd9ec3a56296f4c02179117c0ca5d946b9
Voxfy <55506883+Valdouz@users.noreply.github.com>
2021-01-18 16:56:03 +0100

    Update server-info.js

--- Commandes/server-info.js
+++ Commandes/server-info.js
@@ -7,13 +7,9 @@ const { MessageEmbed } = require('discord.js');
 exports.run = (client, message) => {
 
   const embed = new Discord.MessageEmbed()
-  .setAuthor(message.guild.name, message.guild.iconURL)
   .setColor(3447003)
-  .setDescription(`Owner: ${message.guild.owner.user.tag} (${message.guild.owner.id})`)
-  .addField('AFK Timeout', `${message.guild.afkTimeout / 60} minutes`, true)
-  .addField('AFK Channel', `${message.guild.afkChannelID === null ? 'No AFK Channel' : client.channels.get(message.guild.afkChannelID).name} (${message.guild.afkChannelID === null ? '' : message.guild.afkChannelID})`, true)
-  .addField('Location', message.guild.region, true)
-  .addField('Created', message.guild.createdAt.toLocaleString(), true)
+  .setDescription
+  .addField
   .addBlankField(true)
   .setTimestamp()
   .setFooter(client.user.username, client.user.avatarURL);
