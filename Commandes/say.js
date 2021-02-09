241acaa0834fa6cc2e5d21a3b134041b07e15e7f
LeGeek01 <54872374+LeGeek01@users.noreply.github.com>
2021-02-09 21:15:31 +0100

    Update say.js
    Ajout d'un message d'erreur si l'utilisateur exécutant la commande n'est pas le propriétaire du bot

--- Commandes/say.js
+++ Commandes/say.js
@@ -3,7 +3,10 @@ const config = require("../config.js");
 
 module.exports.run = (client, message, args) => {
 
-if(message.author.id !== config.OWNER) return;
+if(message.author.id !== config.OWNER) {
+    message.channel.send("Vous n'êtes pas le propriétaire du bot !")
+    return;
+}
 let msg = args.join(" ")
 if(!msg) return message.channel.send("Je ne sais pas ce que je dois dire !")
 message.delete()
