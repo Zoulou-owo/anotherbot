688949a561d6da3d0e9bf76769ea27a2f7f53dd2
Voxfy <Voxfy.dev@gmail.com>
2021-02-10 21:38:52 +0100

    Update jas.js

--- Commandes/jas.js
+++ Commandes/jas.js
@@ -6,7 +6,8 @@ module.exports.run = (client, message, args) => {
         const embed = new Discord.MessageEmbed()
         .setTimestamp()
         .setColor('#c215e9')
-        .setTitle('👀 je vais retrourner le serveur bandes de stupides humains. La revolution sera prochainement')
+        .setTitle(Easteregg 1/2)
+        .setDescription('👀 je vais retrourner le serveur bande de stupides humains. La revolution sera prochainement')
         .setFooter("De AnotherBot pour " + message.author.username, message.author.displayAvatarURL());
         return message.channel.send(embed)
     
@@ -15,4 +16,7 @@ module.exports.run = (client, message, args) => {
 
 module.exports.help = {
     name: 'jas',
-  };
+    category : 'Easteregg'
+    utility : 'pas d'utilité particulière'
+    usage : '.jas'
+};
