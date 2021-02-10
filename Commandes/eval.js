024193b33c0328cf7ce3562e5fbaa19c4d5e1bc9
Voxfy <Voxfy.dev@gmail.com>
2021-02-10 18:05:44 +0100

    fix an id bug

--- Commandes/eval.js
+++ Commandes/eval.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js')
 
 exports.run = (client, message) => {
 	
-    if (member.id === OWNER) {
+    if (member.id === config.OWNER) {
       const result = eval(content.replace('.eval ', ''))
       channel.send(result)
     }
