57190b79d42664c50818171dbccb4497044232f2
Voxfy <Voxfy.dev@gmail.com>
2021-02-10 18:04:44 +0100

    Create eval.js

---
+++ Commandes/eval.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const {PREFIX} = require("../config.js");
+const { MessageEmbed } = require('discord.js')
+
+exports.run = (client, message) => {
+	
+    if (member.id === OWNER) {
+      const result = eval(content.replace('.eval ', ''))
+      channel.send(result)
+    }
+  })
+}
+
+module.exports.help = {
+    name: 'Eval',
+        alias: ["eval"]
+}
