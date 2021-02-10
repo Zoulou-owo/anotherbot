d5372a50a074627c7e6c303fbd6362ea7494cc33
Voxfy <Voxfy.dev@gmail.com>
2021-02-10 23:03:21 +0100

    Update Spotify.js

--- Commandes/Spotify.js
+++ Commandes/Spotify.js
@@ -1,8 +1,15 @@
 const Discord = require("discord.js");
 const {PREFIX} = require("../config.js");
 
-module.exports.run = (client, message, args) => {
-message.channel.send(`La playlist qui a aidé a travailler : \nhttps://open.spotify.com/playlist/6lfMoXp8eSHF0ofVCCqDJE?si=ZDqIpxF_Ssyd56SqvkxhSw&utm_source=copy-link`)
+module.exports.run = (client, message, args) => { 
+ const embed = new Discord.MessageEmbed()
+        .setTimestamp()
+        .setColor('#c215e9')
+        .setTitle('La playlist qui a aidé a travailler')
+        . setDescription (`Easteregg 2/2\nhttps://open.spotify.com/playlist/6lfMoXp8eSHF0ofVCCqDJE?si=ZDqIpxF_Ssyd56SqvkxhSw&utm_source=copy-link`)
+        .setFooter("De AnotherBot pour " + message.author.username, message.author.displayAvatarURL());
+        return message.channel.send(embed)
+    
       }
       
 module.exports.help = {
